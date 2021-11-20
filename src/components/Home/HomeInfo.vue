85d0356d18827deac8620d2f33b21a79a4f44a53
lavsemen <remark.lavrentev@gmail.com>
2021-11-20 14:15:33 +0500

    create voice input

---
+++ src/components/Home/HomeInfo.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home__info">
+        <div class="home__city">
+          {{ city }}, {{ country }}
+        </div>
+        <div class="home__date">{{date}}</div>
+        <div class="home__temperature">{{temp}}&#176;С</div>
+        <div class="home__type">{{type}}</div>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeInfo',
+  props: {
+    city: {
+      type: String,
+      required: true
+    },
+    country: {
+      type: String,
+      required: true
+    },
+    date: {
+      type: String,
+      required: true
+    },
+    temp: {
+      type: Number,
+      required: true
+    },
+     type: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,44 @@
 <template>
   <div class="home">
     <div class="home__content">
-      <div class="home__search">
-        <input
-          type="text"
-          placeholder="Search..."
-          v-model.trim="searchInput"
-          @keyup.enter="fetchWeather"
-        />
-      </div>
-      <div class="home__info" v-if="cityValid">
-        <div class="home__city">
-          {{ cityInfo.name }}, {{ cityInfo.country }}
-        </div>
-        <div class="home__date">{{date}}</div>
-        <div class="home__temperature">{{cityInfo.temp}}&#176;С</div>
-        <div class="home__type">{{cityInfo.type}}</div>
-      </div>
+      <home-search
+        @voiceInput="voiceInput"
+        @fetchWether="fetchWeather"
+        :search-input="searchInput"
+        :voice-active="voiceActive"
+      />
+      <home-info
+        v-if="cityValid"
+        :type="cityInfo.type"
+        :temp="cityInfo.temp"
+        :city="cityInfo.name"
+        :date="date"
+        :country="cityInfo.country"
+      />
+      <h1 class="home__title" v-else>
+        Хотите узнать погоду? <br />
+        Просто напиши название города.
+      </h1>
     </div>
   </div>
 </template>
 
 <script>
-import { reactive, ref, toRefs, computed } from "vue";
+import { reactive, ref, toRefs, computed, onMounted } from "vue";
 import { API_KEY } from "@/API/WeatherApi";
 import axios from "axios";
+import HomeInfo from "@/components/Home/HomeInfo.vue";
+import HomeSearch from '../components/Home/HomeSearch.vue';
 
 export default {
+  components: { HomeInfo, HomeSearch },
   name: "Home",
   setup(props, context) {
     const searchInput = ref("");
     const cityInfo = reactive({});
     const cityValid = ref(false);
-    const date = (new Date).toLocaleDateString()
+    const date = new Date().toLocaleDateString();
+    const voiceActive = ref(false)
 
     const fetchWeather = async () => {
       try {
@@ -46,15 +52,38 @@ export default {
             return;
           }
           console.log(res.data);
-          cityInfo.name = res.data.name
-          cityInfo.country = res.data.sys.country
-          cityInfo.temp = Math.round(res.data.main.temp)
-          cityInfo.type = res.data.weather[0].main
-          cityValid.value = true
+          cityInfo.name = res.data.name;
+          cityInfo.country = res.data.sys.country;
+          cityInfo.temp = Math.round(res.data.main.temp);
+          cityInfo.type = res.data.weather[0].main;
+          cityValid.value = true;
         }
       } catch (err) {
         cityValid.value = false;
       }
+    };
+
+    const voiceInput = (event) => {
+      const SpeechRecognition =
+        window.SpeechRecognition || window.webkitSpeechRecognition;
+      const SpeechGrammarList =
+        window.SpeechGrammarList || window.webkitSpeechGrammarList;
+      const SpeechRecognitionEvent =
+        window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent;
+
+      const recognition = new SpeechRecognition();
+      recognition.start();
+
+      recognition.onaudiostart = () => {
+        voiceActive.value = true
+      };
+      recognition.onresult = async (e) => {
+        voiceActive.value = false
+        searchInput.value = await e.results[0][0].transcript;
+        fetchWeather();
+      };
+
+    
     };
 
     return {
@@ -63,86 +92,13 @@ export default {
       cityValid,
       date,
       fetchWeather,
+      voiceActive,
+      voiceInput,
     };
   },
 };
 </script>
 
-<style lang="scss" scoped>
-.home {
-  background: url("../assets/home-bg.gif");
-  height: 100vh;
-  position: relative;
-  z-index: 0;
-  &::after {
-    content: "";
-    display: block;
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background: rgba(#000, 0.3);
-    z-index: 0;
-  }
-  &__content {
-    padding-top: 50px;
-    position: relative;
-    z-index: 1;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    width: 100%;
-  }
-  &__search {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    input {
-      background: rgba(#fff, 0.7);
-      border-radius: 8px 0 8px 0;
-      width: calc(100% - 20px);
-      max-width: 500px;
-      height: 40px;
-      padding-left: 10px;
-      font-size: 19px;
-      border: 0;
-      color: #000;
-      cursor: pointer;
-    }
-  }
-  &__city {
-    font-weight: 600;
-    font-size: 48px;
-    margin-top: 20px;
-    text-shadow: 7px 7px 9px rgba(0, 0, 0, 0.44);
-  }
-  &__date {
-    font-size: 24px;
-    margin-top: 8px;
-    font-style: italic;
-    font-weight: 300;
-  }
-  &__info {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-  }
-  &__temperature {
-    padding: 20px;
-    background: rgba(rgb(181, 185, 189), 0.6);
-    border-radius: 8px;
-    box-shadow: 7px 7px 40px rgba(0, 0, 0, 0.32);
-    margin-top: 24px;
-    font-weight: 700;
-    text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.44);
-    font-size: 72px;
-  }
-  &__type {
-    margin-top: 20px;
-    font-style: italic;
-    font-weight: 700;
-    font-size: 42px;
-  }
-}
+<style lang="scss">
+@import "@/assets/scss/home.scss";
 </style>
