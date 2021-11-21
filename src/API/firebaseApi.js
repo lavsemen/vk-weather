105eb1f5372ee18434cda75a1103812120ad225a
lavsemen <remark.lavrentev@gmail.com>
2021-11-22 00:24:54 +0500

    fix

--- src/API/firebaseApi.js
+++ src/API/firebaseApi.js
@@ -1,5 +1,5 @@
-const URL_USERS = 'https://vue-with-http-620fc-default-rtdb.europe-west1.firebasedatabase.app/users.json'
-const GET_USER = 'https://vue-with-http-620fc-default-rtdb.europe-west1.firebasedatabase.app/users/'
+const URL_USERS = 'https://vk-wather-default-rtdb.europe-west1.firebasedatabase.app/users.json'
+const GET_USER = 'https://vk-wather-default-rtdb.europe-west1.firebasedatabase.app/users/'
 export {
   URL_USERS,
   GET_USER
